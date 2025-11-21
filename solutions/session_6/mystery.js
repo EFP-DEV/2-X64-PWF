e9586066ad76cd43f855da7c8a711af39ccb0414
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-11-21 02:36:40 +0100

    added session 7 solutions

--- solutions/session_6/mystery.js
+++ solutions/session_6/mystery.js
@@ -1,20 +1,33 @@
 let btn = document.getElementById('guess');
+let input = document.getElementById('answer');
+
 let mystery = Math.floor(Math.random() * 10) + 1;
 
-console.log('Triche: ' + mystery)
+console.log('Triche: ' + mystery);
+
+input.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Enter') {
+    play(input, mystery);
+  }
+});
 
 btn.addEventListener('click', () => {
-    let input = document.getElementById('answer');
-    let output = document.getElementById('message');
-
-    if(input.value == mystery){
-        output.textContent = 'WON';
-        output.classList.add('success');
-        output.classList.remove('failure');
-    }
-    else{
-      output.innerHTML = 'LOST';
-      output.classList.add('failure');
-      output.classList.remove('success');
-    }
+  play(input, mystery);
 });
+
+function play(input, mystery) {
+  let output = document.getElementById('message');
+
+  if (input.value == mystery) {
+    output.textContent = 'WON';
+    output.classList.add('success');
+    output.classList.remove('failure');
+  } else {
+    output.innerHTML = 'LOST';
+    output.classList.add('failure');
+    output.classList.remove('success');
+
+    input.value = '';
+    input.focus();
+  }
+}
