9c6b9f95edf8b35711b1aac28a3894fef4223e3e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-11-28 14:12:41 +0100

    X64A-step 1

--- solutions/session_8/counter-dom.js
+++ solutions/session_8/counter-dom.js
@@ -1,23 +1,7 @@
-console.log('coucou');
-
-document.addEventListener('click', function (event) {
-
+document.addEventListener('click', function(event){
   if(event.target.classList.contains('count')){
-    console.log('Click +1 ');
-    let btn = event.target;
-    let article = btn.closest('article');
-    let counter = article.querySelector('.value')
+    let article = event.target.closest('article');
+    let counter = article.querySelector('.value');
     counter.textContent = 1 + parseInt(counter.textContent);
   }
-  else if(event.target.getAttribute('id') == 'reset'){
-    console.log('Click reset');
-    let values = document.querySelectorAll('.value');
-    console.log(values);
-    let index = 0;
-    while(index < values.length){
-      values[index].textContent = 0;
-      index = index + 1;
-    }
-  }
-
 });
