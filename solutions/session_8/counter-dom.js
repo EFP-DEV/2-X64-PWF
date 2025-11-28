5e2fe1c22b32d06cbf316b8eb1d42ee253f54586
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-11-28 14:26:27 +0100

    x64A part 2

--- solutions/session_8/counter-dom.js
+++ solutions/session_8/counter-dom.js
@@ -1,7 +1,17 @@
-document.addEventListener('click', function(event){
-  if(event.target.classList.contains('count')){
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('count')) {
     let article = event.target.closest('article');
     let counter = article.querySelector('.value');
     counter.textContent = 1 + parseInt(counter.textContent);
+  } else if (event.target.getAttribute('id') == 'reset') {
+    
+    // handle reset
+    let values = document.querySelectorAll('.value');
+    let index = 0;
+
+    while(index < values.length){
+      values[index].textContent = 0;
+      index = index + 1
+    }
   }
 });
