4fbc4c69c48cabdaf2de52c48f96cb91d9998c0e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-12-05 14:46:27 +0100

    Increase cat image fetch limit from 5 to 10

--- exercices/session_10/catsanddogs.js
+++ exercices/session_10/catsanddogs.js
@@ -3,7 +3,7 @@ let gallery = document.querySelector('#gallery');
 
 btn.addEventListener('click', () => {
 
-  fetch('https://api.thecatapi.com/v1/images/search?limit=5')
+  fetch('https://api.thecatapi.com/v1/images/search?limit=10')
     .then(response => response.json())
     .then(cats => {
       console.log('nombre de chats, cats.length, 'chats', cats)
